68ad893ea6d2f0b66feb085280a72f7872ad1d74
Mike Heffner <mikeh@fesnel.com>
2012-12-18 16:52:31 -0500

    Oops, don't lose time conversion to epoch seconds.

--- services/libratometrics.rb
+++ services/libratometrics.rb
@@ -11,7 +11,7 @@ class Service::LibratoMetrics < Service
     end
 
     payload[:events].each do |event|
-      time = Time.parse(event[:received_at])
+      time = Time.parse(event[:received_at]).to_i
       values[event[:source_name]][time] += 1
     end
 
