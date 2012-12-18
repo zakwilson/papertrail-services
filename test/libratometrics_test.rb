19cc5614e9a631cd3f0de54174f7678a02937c54
Mike Heffner <mikeh@fesnel.com>
2012-12-18 16:08:46 -0500

    Shift the payload timestamps to within an acceptable range for the Librato gem.

--- test/libratometrics_test.rb
+++ test/libratometrics_test.rb
@@ -9,7 +9,7 @@ class LibratoMetricsTest < PapertrailServices::TestCase
     Librato::Metrics::Client.any_instance.expects(:fetch).raises(Librato::Metrics::NotFound)
     Librato::Metrics::Queue.any_instance.expects(:submit)
 
-    svc = service(:logs, { :name => 'gauge', :user => 'a@something.com', :token => 'abc' }, payload)
+    svc = service(:logs, { :name => 'gauge', :user => 'a@something.com', :token => 'abc' }, shifted_payload)
 
     @stubs.post '/v1/metrics.json' do |env|
       [200, {}, '']
@@ -22,7 +22,7 @@ class LibratoMetricsTest < PapertrailServices::TestCase
     Librato::Metrics::Client.any_instance.expects(:fetch).returns({ 'source1' => { :count => 1, :measure_time => Time.now.to_i }})
     Librato::Metrics::Queue.any_instance.expects(:submit)
 
-    svc = service(:logs, { :name => 'gauge', :user => 'a@b.com', :token => 'abc' }, payload)
+    svc = service(:logs, { :name => 'gauge', :user => 'a@b.com', :token => 'abc' }, shifted_payload)
 
     @stubs.post '/v1/metrics.json' do |env|
       [200, {}, '']
@@ -35,7 +35,7 @@ class LibratoMetricsTest < PapertrailServices::TestCase
   def test_unauthorized
     Librato::Metrics::Client.any_instance.expects(:fetch).raises(Librato::Metrics::Unauthorized)
 
-    svc = service(:logs, { :name => 'gauge', :user => 'a@b.com', :token => 'abc' }, payload)
+    svc = service(:logs, { :name => 'gauge', :user => 'a@b.com', :token => 'abc' }, shifted_payload)
 
     assert_raise Service::ConfigurationError do
       svc.receive_logs
@@ -45,11 +45,29 @@ class LibratoMetricsTest < PapertrailServices::TestCase
   def test_error
     Librato::Metrics::Client.any_instance.expects(:fetch).raises(Librato::Metrics::MetricsError)
 
-    svc = service(:logs, { :name => 'gauge', :user => 'a@b.com', :token => 'abc' }, payload)
+    svc = service(:logs, { :name => 'gauge', :user => 'a@b.com', :token => 'abc' }, shifted_payload)
 
     assert_raise Service::ConfigurationError do
       svc.receive_logs
     end
+  end
+
+  # Shift time stamps to within the acceptable offset from
+  # real time. This should try to keep the uniqueness of times
+  # whenever possible (except when a factor of the max offset).
+  def shifted_payload
+    now = Time.now.tv_sec
+    max_real_offset = 3600 * 24
+
+    spayload = payload.dup
+    spayload[:events].each do |event|
+      time = Time.parse(event[:received_at])
+      time = time.to_i - (time.to_i % 60)
+      delta = now - time
+      event[:received_at] = Time.at(now - (delta % max_real_offset)).to_s
+    end
+
+    spayload
   end
 
   def service(*args)
