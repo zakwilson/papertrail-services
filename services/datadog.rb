1e2662d40d6b21299381e07123b029adda70497a
Eric Lindvall <eric@5stops.com>
2013-03-09 22:53:10 -0800

    Add datadog hook

---
+++ services/datadog.rb
@@ -0,0 +1,45 @@
+# encoding: utf-8
+class Service::Datadog < Service
+  def receive_logs
+    raise_config_error 'Missing API Key' if settings[:api_key].to_s.empty?
+    raise_config_error 'Missing metric name' if settings[:metric].to_s.empty?
+
+    unless settings[:tags].to_s.empty?
+      tags = settings[:tags].to_s.split(/,\s+/)
+    end
+
+    # values[hostname][time]
+    values = Hash.new do |h,k|
+      h[k] = Hash.new do |i,l|
+        i[l] = 0
+      end
+    end
+
+    payload[:events].each do |event|
+      time = Time.iso8601(event[:received_at]).to_i
+      values[event[:source_name]][time] += 1
+    end
+
+    serieses = []
+
+    values.each do |hostname, points|
+      serieses << {
+        :metric => settings[:metric],
+        :points => points.to_a,
+        :host => hostname,
+        :tags => tags,
+        :type => 'counter'
+      }
+    end
+
+    http_post "https://app.datadoghq.com/api/v1/series" do |req|
+      req.params = {
+        :api_key => settings[:api_key]
+      }
+
+      req.body = {
+        :series => serieses
+      }.to_json
+    end
+  end
+end
---
+++ test/datadog_test.rb
@@ -0,0 +1,32 @@
+require File.expand_path('../helper', __FILE__)
+
+class DatadogTest < PapertrailServices::TestCase
+  def setup
+    @stubs = Faraday::Adapter::Test::Stubs.new
+  end
+
+  def test_config
+    svc = service(:logs, {}.with_indifferent_access, payload)
+    assert_raises(PapertrailServices::Service::ConfigurationError) { svc.receive_logs }
+
+    svc = service(:logs, { 'api_key' => "foobar" }.with_indifferent_access, payload)
+    assert_raises(PapertrailServices::Service::ConfigurationError) { svc.receive_logs }
+
+    svc = service(:logs, { 'metric' => "foobar" }.with_indifferent_access, payload)
+    assert_raises(PapertrailServices::Service::ConfigurationError) { svc.receive_logs }
+  end
+
+  def test_logs
+    svc = service(:logs, { 'api_key' => 'foobar', "metric" => "foo.bar"}.with_indifferent_access, payload)
+
+    @stubs.post "/api/v1/series" do |env|
+      [200, {}, ""]
+    end
+
+    svc.receive_logs
+  end
+
+  def service(*args)
+    super Service::Datadog, *args
+  end
+end
