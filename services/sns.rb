536426981e608780b484e997a4ca46fd437147de
Eric Lindvall <eric@5stops.com>
2014-06-23 15:00:06 -0700

    Add error checking for params for SNS

--- services/sns.rb
+++ services/sns.rb
@@ -1,9 +1,16 @@
 class Service::SNS < Service
   def receive_logs
+    raise_config_error 'Missing AWS Access Key' if settings[:aws_access_key_id].to_s.empty?
+    raise_config_error 'Missing AWS Secret Access Key' if settings[:aws_secret_access_key].to_s.empty?
+    raise_config_error 'Missing AWS Region' if settings[:aws_region].to_s.empty?
+    raise_config_error 'Missing AWS SNS Topic' if settings[:aws_sns_topic_arn].to_s.empty?
+
+
     sns = AWS::SNS.new(
-    :access_key_id => settings[:aws_access_key_id],
-    :secret_access_key => settings[:aws_secret_access_key],
-    :region => settings[:aws_region])
+      :access_key_id => settings[:aws_access_key_id],
+      :secret_access_key => settings[:aws_secret_access_key],
+      :region => settings[:aws_region])
+
     topic = sns.topics[settings[:aws_sns_topic_arn]]
 
     payload[:events].each do |event|
