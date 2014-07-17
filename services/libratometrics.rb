d472f38c46c2f2fec67aceb89363b7c396192517
Larry Marburger <larry@marburger.cc>
2014-07-17 13:58:17 -0400

    Preserve receive logs behavior

--- services/libratometrics.rb
+++ services/libratometrics.rb
@@ -3,6 +3,52 @@ class Service::LibratoMetrics < Service
   def receive_logs
     name = settings[:name].gsub(/ +/, '_')
 
+    # values[hostname][time]
+    values = Hash.new do |h,k|
+      h[k] = Hash.new do |i,l|
+        i[l] = 0
+      end
+    end
+
+    payload[:events].each do |event|
+      time = Time.iso8601(event[:received_at]).to_i
+      time = time.to_i - (time.to_i % 60)
+      values[event[:source_name]][time] += 1
+    end
+
+    client = Librato::Metrics::Client.new
+    client.authenticate(settings[:user].to_s.strip, settings[:token].to_s.strip)
+    client.agent_identifier("Papertrail-Services/1.0")
+
+    queue = client.new_queue
+
+    values.each do |source_name, hash|
+      hash.each do |time, count|
+        queue.add name => {
+          :source       => source_name,
+          :value        => count,
+          :measure_time => time,
+          :type         => 'gauge'
+        }
+      end
+    end
+
+    unless queue.empty?
+      queue.submit
+    end
+  rescue Librato::Metrics::ClientError => e
+    if e.message !~ /is too far in the past/
+      raise_config_error("Error sending to Librato Metrics: #{e.message}")
+    end
+  rescue Librato::Metrics::CredentialsMissing, Librato::Metrics::Unauthorized
+    raise_config_error("Error sending to Librato Metrics: Invalid email address or token")
+  rescue Librato::Metrics::MetricsError => e
+    raise_config_error("Error sending to Librato Metrics: #{e.message}")
+  end
+
+  def receive_counts
+    name = settings[:name].gsub(/ +/, '_')
+
     values = {}
     payload[:counts].each do |count|
       values[count[:source_name]] = count[:timeseries].
--- test/libratometrics_test.rb
+++ test/libratometrics_test.rb
@@ -4,7 +4,7 @@ class LibratoMetricsTest < PapertrailServices::TestCase
   def test_logs
     Librato::Metrics::Queue.any_instance.expects(:submit)
 
-    svc = service(:logs, { :name => 'gauge', :user => 'a@b.com', :token => 'abc' }, shifted_payload)
+    svc = service(:logs, { :name => 'gauge', :user => 'a@b.com', :token => 'abc' }, shifted_logs_payload)
 
     http_stubs.post '/v1/metrics.json' do |env|
       [200, {}, '']
@@ -13,43 +13,86 @@ class LibratoMetricsTest < PapertrailServices::TestCase
     svc.receive_logs
   end
 
-
-  def test_unauthorized
+  def test_logs_unauthorized
     Librato::Metrics::Queue.any_instance.expects(:submit).raises(Librato::Metrics::Unauthorized.new('unauthorized'))
 
-    svc = service(:logs, { :name => 'gauge', :user => 'a@b.com', :token => 'abc' }, shifted_payload)
+    svc = service(:logs, { :name => 'gauge', :user => 'a@b.com', :token => 'abc' }, shifted_logs_payload)
 
     assert_raise Service::ConfigurationError do
       svc.receive_logs
     end
   end
 
-  def test_error
+  def test_logs_error
     Librato::Metrics::Queue.any_instance.expects(:submit).raises(Librato::Metrics::MetricsError)
 
-    svc = service(:logs, { :name => 'gauge', :user => 'a@b.com', :token => 'abc' }, shifted_payload)
+    svc = service(:logs, { :name => 'gauge', :user => 'a@b.com', :token => 'abc' }, shifted_logs_payload)
 
     assert_raise Service::ConfigurationError do
       svc.receive_logs
+    end
+  end
+
+  def test_counts
+    Librato::Metrics::Queue.any_instance.expects(:submit)
+
+    svc = service(:logs, { :name => 'gauge', :user => 'a@b.com', :token => 'abc' }, shifted_counts_payload)
+
+    http_stubs.post '/v1/metrics.json' do |env|
+      [200, {}, '']
+    end
+
+    svc.receive_counts
+  end
+
+  def test_counts_unauthorized
+    Librato::Metrics::Queue.any_instance.expects(:submit).raises(Librato::Metrics::Unauthorized.new('unauthorized'))
+
+    svc = service(:logs, { :name => 'gauge', :user => 'a@b.com', :token => 'abc' }, shifted_counts_payload)
+
+    assert_raise Service::ConfigurationError do
+      svc.receive_counts
+    end
+  end
+
+  def test_counts_error
+    Librato::Metrics::Queue.any_instance.expects(:submit).raises(Librato::Metrics::MetricsError)
+
+    svc = service(:logs, { :name => 'gauge', :user => 'a@b.com', :token => 'abc' }, shifted_counts_payload)
+
+    assert_raise Service::ConfigurationError do
+      svc.receive_counts
     end
   end
 
   # Shift time stamps to within the acceptable offset from
   # real time. This should try to keep the uniqueness of times
   # whenever possible (except when a factor of the max offset).
-  def shifted_payload
-    now = Time.now.tv_sec
+  def shifted_time(time, now)
     max_real_offset = 3600 * 24
+    shifted_time    = Time.iso8601(time)
+    shifted_time    = shifted_time.to_i - (shifted_time.to_i % 60)
+    delta           = now - shifted_time
+    Time.at(now - (delta % max_real_offset)).iso8601
+  end
 
+  def shifted_logs_payload
+    now     = Time.now.tv_sec
+    shifted = payload.dup
+    shifted[:events].each do |event|
+      event[:received_at] = shifted_time(event[:received_at], now)
+    end
+
+    shifted
+  end
+
+  def shifted_counts_payload
+    now     = Time.now.tv_sec
     shifted = counts_payload.dup
     shifted[:counts].each do |count|
       count[:timeseries] = count[:timeseries].
         each_with_object({}) do |(time, count), timeseries|
-          time  = Time.iso8601(time)
-          time  = time.to_i - (time.to_i % 60)
-          delta = now - time
-          shifted_time = Time.at(now - (delta % max_real_offset)).iso8601
-          timeseries[shifted_time] = count
+          timeseries[shifted_time(time, now)] = count
         end
     end
 
