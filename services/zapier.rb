dbdf1ceba7fdd822ed4b03087a50f820c158d30e
Zak Wilson <zak.wilson@gmail.com>
2015-09-17 17:47:01 -0400

    Add initial support for Zapier

---
+++ services/zapier.rb
@@ -0,0 +1,35 @@
+# coding: utf-8
+
+class Service::Zapier < Service
+  attr_writer :zapier
+
+  size_limit = 5242880
+
+  def json_limited(payload, size_limit)
+    ret = payload.to_json
+
+    while ret.length > size_limit
+      estimate = 0.9 * size_limit / ret.length
+      new_length = (payload[:events].length * estimate).floor
+      payload[:events] = payload[:events][0 .. new_length - 1]
+      ret = payload.to_json
+    end
+
+    ret
+  end
+
+  def receive_logs
+    raise_config_error 'Missing Zapier URL' if
+      settings[:zapier_url].to_s.empty?
+
+    http.headers['content-type'] = 'application/json'
+    resp = http_post settings[:zapier_url], json_limited(payload, size_limit)
+    
+    unless resp.status == 200
+      puts "zapier: #{resp.to_s}"
+ 
+      raise_config_error "Failed to post to Zapier"
+    end
+  end
+
+end
---
+++ test/zapier_test.rb
@@ -0,0 +1,35 @@
+require File.expand_path('../helper', __FILE__)
+
+class ZapierTest < PapertrailServices::TestCase
+
+  def test_config
+    svc = service(:logs, {:zapier_url => ''}, payload)
+    assert_raises(PapertrailServices::Service::ConfigurationError) { svc.receive_logs }
+  end
+
+  def test_size
+    # This assumes the sample payload as of this writing, with a lize of 1743
+    
+    svc = service(:logs, {:zapier_url => 'https://zapier.com/hooks/catch/sample_url/'},
+                  payload)
+    limited_payload = svc.json_limited(payload, 1400)
+    assert(limited_payload.length < 1400)
+  end
+  
+  def test_logs
+    svc = service(:logs, {:zapier_url => 'https://zapier.com/hooks/catch/sample_url/'},
+                  payload)
+
+    http_stubs.post 'https://zapier.com/hooks/catch/sample_url/' do |env|
+      [200, {:content_type => "application/json"}, { :status => 1 }.to_json]
+
+      svc.receive_logs
+    end 
+
+  end
+
+  def service(*args)
+    super Service::Zapier, *args
+  end
+  
+end
